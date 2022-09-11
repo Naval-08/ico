53f2d94bb4f0bd76cb57e440194ffb3888977621
naval-08 <ndvnaug25@gmail.com>
2022-09-11 19:50:18 +0530

    initial

---
+++ ICO/hardhat-tutorial/ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x0720E89d10bE2EC8Cff0cDeBca92afF9e5d7eFDf";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
